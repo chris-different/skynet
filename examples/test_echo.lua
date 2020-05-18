6f7051884caa8defbb28153053ad5f1cd586a548
chris-different <532604040@qq.com>
2020-05-18 11:12:04 +0800

    2

---
+++ examples/test_echo.lua
@@ -0,0 +1,7 @@
+local skynet = require "skynet"
+
+skynet.start(function()
+    local echo = skynet.newservice("echo")
+    print("start christ")
+    print(skynet.call(echo, "lua", "HELLO", "world"))
+end);
